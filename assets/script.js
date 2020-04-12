fbd67c00dc1f10c99b77bb9cd1091ac674d69743
Diab <emailcodydiab@gmail.com>
2020-04-12 11:29:41 -0500

    vurther debugg, added style and image

--- assets/script.js
+++ assets/script.js
@@ -52,6 +52,7 @@
 //  var quizBoxEl = document.createElement("div").className = "quiz-box"
 
 var score = 0;
+
 var quizPitch= document.getElementById("quiz-content")
 var startButton = document.getElementById("start-button-container");
 var choiceOne =  document.getElementById("A");
@@ -59,8 +60,32 @@ var choiceTwo = document.getElementById("B");
 var choiceThree = document.getElementById("C");
 let currentQuestion = 0;
 let lastQuestion = questions.length-1
+var timer = document.getElementById("timer")
 
 
+function timeCount() {
+    
+    timerInterval = setInterval(function() {
+    
+        timerSecs --;
+        timer.textContent ="Time: " +timerSecs + " seconds";
+
+        
+        if (timerSecs < 1) {
+            timer.textContent = 0;
+            clearInterval(timerInterval);
+            alert("Times Up!")
+            
+            displayScore()
+        };
+        
+        // clear timer if current question is 5
+        if (currentQuestion === 4) {
+            timer.textContent = timerSecs;
+            clearInterval(timerInterval);
+        }
+    }, 1000)
+}
     
        
 function displayQuestion(){
@@ -80,8 +105,27 @@ function displayQuestion(){
 }
 
 function displayScore(){
-quizPitch.setAttribute("style","display:none")
-console.log(score)
+   quizPitch.setAttribute("style","display:none")
+   startButton.setAttribute("style","display:flex")
+    var finalScore = score * 100
+    timerInterval=null
+alert("Your final score is " + finalScore + "!")
+
+var highScore = localStorage.getItem("highscore")
+if (parseInt(highScore) < finalScore || !highScore){
+    alert("You have the new highscore of "+finalScore+ "! The Sphynx is satisfied" )
+    localStorage.setItem("highscore",finalScore)
+    
+}if (parseInt(highScore) = finalScore){
+    alert("You have tied for the highscore of " +highScore+ " . Narrowly escaping the Sphynx" )
+
+}else{
+    alert("Good effort, but you didnt beat the highscore of " +highScore+ ". The Sphynx ate you!!! ")
+    
+}
+ //compare to highscore in local storage
+ //if higher || no high score set to local storage && "you have the highscore of "
+ //if less sorry you didnt beet the high score of 
 }
  
 function start(){
@@ -89,7 +133,10 @@ function start(){
     
     
     startButton.setAttribute("style","display:none");
-    document.getElementsByClassName("score").textContent="Your current score is"  + score + " !";
+    timerSecs=10
+    //timer.textContent ="Time: " +timerSecs+ " seconds";
+
+    
     displayQuestion();
 
     
@@ -111,6 +158,7 @@ function checkAnswer(answer){
         currentQuestion++;
         start()
     }else{
+        displayScore()
 
     }
 }
